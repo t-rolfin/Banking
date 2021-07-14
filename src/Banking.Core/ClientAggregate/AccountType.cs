91348e1eb763c6a44e23cc7d8b2700c8cc024c02
t-rolfin <rolfin.dc@gmail.com>
2021-07-14 23:48:26 +0300

    Added classes for AccountType.

--- src/Banking.Core/ClientAggregate/AccountType.cs
+++ src/Banking.Core/ClientAggregate/AccountType.cs
@@ -14,5 +14,44 @@ namespace Banking.Core.ClientAggregate
         }
 
         public string Name { get; init; }
+        public List<Operation> Operations { get; }
+
+        public void AddOperation(OperationType operationType, Commission commission)
+        {
+            this.Operations.Add(
+                    new Operation(operationType, commission)
+                );
+        }
+    }
+
+    public class Operation
+    {
+        public Operation(OperationType operationType, Commission commission)
+        {
+            OperationType = operationType;
+            Commission = commission;
+        }
+
+        public OperationType OperationType { get; set; }
+        public Commission Commission { get; set; }
+    }
+
+    public class Commission
+    {
+        public Commission(float percent, float @fixed)
+        {
+            Percent = percent;
+            Fixed = @fixed;
+        }
+
+        public float Percent { get; set; }
+        public float Fixed { get; set; }
+    }
+
+    public enum OperationType
+    {
+        Withdrawal = 0,
+        Deposit = 1,
+        Transfer = 2,
     }
 }
