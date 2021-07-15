28547f60bd187c86ea86e9fcdc59a6a12c360461
t-rolfin <rolfin.dc@gmail.com>
2021-07-15 20:16:34 +0300

    Added new tests for Account and Client.

---
+++ tests/Banking.Tests/AccountTests.cs
@@ -0,0 +1,70 @@
+﻿using AutoFixture;
+using Banking.Core.AccountTypeFactory;
+using Banking.Core.Entities;
+using Banking.Core.Exceptions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Banking.Tests
+{
+    public class AccountTests
+    {
+
+        private Account _sut;
+        private Account _sutGold;
+
+        public AccountTests()
+        {
+            var fixture = new Fixture();
+
+            _sut = fixture.Build<Account>()
+                .With(x => x.AccountType, new BasicAccountType().GetAccountType())
+                .Create();
+
+            _sutGold = fixture.Build<Account>()
+                .With(x => x.AccountType, new GoldAccountType().GetAccountType())
+                .Create();
+        }
+
+        [Fact]
+        public void Basic_ShouldThrowInsufficientAmountExceptionWhenAmountIsToLow()
+        {
+            Func<decimal> func = () => _sut.Withdrawal(300);
+            func.Should().Throw<InsufficientAmountException>();
+        }
+
+        [Fact]
+        public void Basic_ShouldTakeTheCommissionWhenDeposit()
+        {
+            decimal expect = 297;
+
+            _sut.Deposit(300);
+
+            _sut.Amount.Should().Be(expect);
+        }
+
+        [Fact]
+        public void Basic_ShoulAddTransactionWheDepositAnAmountOfMoney()
+        {
+            _sut.Deposit(300);
+
+            _sut.Transactions.Count().Should().Be(1);
+        }
+
+        [Fact]
+        public void Gold_ShouldNotTakeTheCommissionWhenDeposit()
+        {
+            int expect = 789;
+
+            _sutGold.Deposit(789);
+
+            _sutGold.Amount.Should().Be(expect);
+        }
+
+    }
+}
--- tests/Banking.Tests/ClientTests.cs
+++ tests/Banking.Tests/ClientTests.cs
@@ -7,7 +7,6 @@ namespace Banking.Tests
 {
     public class ClientTests
     {
-
         private readonly Client _sut;
 
         public ClientTests()
@@ -22,6 +21,7 @@ namespace Banking.Tests
 
             _sut.CreateAccount(
             new Account(
+                    "1928489323845",
                     "RO49AAAA1B31007593840000",
                     accountType,
                     Core.Shared.CurrencyType.RON
@@ -29,6 +29,34 @@ namespace Banking.Tests
 
 
             _sut.Accounts.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void ShouldChangeThePINWhenChangePINIsCalled()
+        {
+            var newPIN = "2482";
+
+            _sut.ChangePIN(newPIN);
+
+            _sut.PIN.Should().Be(newPIN);
+        }
+
+        [Fact]
+        public  void ShouldSetAccoutIsClosedToTrueWheCallCloseAccount()
+        {
+            AccountType accountType = new AccountType("Gold");
+
+            _sut.CreateAccount(
+            new Account(
+                    "1928489323845",
+                    "RO49AAAA1B31007593840000",
+                    accountType,
+                    Core.Shared.CurrencyType.RON
+                ));
+
+            _sut.Accounts[0].CloseAccount();
+
+            _sut.Accounts[0].IsClosed.Should().BeTrue();
         }
     }
 }
