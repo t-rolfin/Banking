d6984321336f0a134bddd34986f750db687c4161
t-rolfin <rolfin.dc@gmail.com>
2021-07-16 19:54:57 +0300

    Added IBANGenerator.

---
+++ src/Banking.Shared/Helpers/IBANGenerator.cs
@@ -0,0 +1,22 @@
+﻿using SinKien.IBAN4Net;
+using System;
+
+namespace Banking.Shared.Helpers
+{
+    public static class IBANGenerator
+    {
+        public static string Generate()
+        {
+            var random = new Random();
+
+            Iban iban = new IbanBuilder()
+                .CountryCode(CountryCode.GetCountryCode("RO"))
+                .BankCode("dsfa".ToUpper())
+                .AccountNumberPrefix(random.Next(100, 999).ToString())
+                .AccountNumber($"200014539947{random.Next(1000, 9999)}")
+                .Build(true, false);
+
+            return iban.ToString();
+        }
+    }
+}
