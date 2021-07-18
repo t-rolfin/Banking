b44d665c4ccf3152397003ee32b008cd72fdda93
t-rolfin <rolfin.dc@gmail.com>
2021-07-18 15:30:42 +0300

    Created SignIn page.

---
+++ src/Banking.Shared/Models/SignInModel.cs
@@ -0,0 +1,48 @@
+﻿using Banking.Shared.Enums;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Shared.Models
+{
+    public class SignInModel
+    {
+        public SignInModel()
+        { }
+
+        public SignInModel(string cNP, string pIN, string pINConfig, string firstName, 
+            string lastName, string address, CurrencyType currencyType, AccountTypeEnum accountType)
+        {
+            CNP = cNP;
+            PIN = pIN;
+            PINConfig = pINConfig;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            CurrencyType = currencyType;
+            AccountType = accountType;
+        }
+
+        [MaxLength(13, ErrorMessage = "The CNP must have a length of 13.")]
+        [MinLength(13, ErrorMessage = "The CNP must have a length of 13.")]
+        public string CNP { get; set; }
+
+        [DataType(DataType.Password)]
+        public string PIN { get; set; }
+
+        [Compare("PIN")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Pin Confirm")]
+        public string PINConfig { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public CurrencyType CurrencyType { get; set; } = CurrencyType.RON;
+        public AccountTypeEnum AccountType { get; set; } = AccountTypeEnum.Basic;
+    }
+}
---
+++ src/Banking.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Banking.Shared.Models;
+using Banking.Core;
+
+namespace Banking.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            var model = new SignInModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SignIn(SignInModel model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
