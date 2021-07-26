513dcbee1ee41574ea0fce82ea0554fd557d8497
t-rolfin <rolfin.dc@gmail.com>
2021-07-26 19:43:52 +0300

    Added account creation by an operator(just ui).

---
+++ src/Banking.Shared/Models/CreateAccountModel.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Shared.Models
+{
+    public class CreateAccountModel
+    {
+        public CreateAccountModel() { }
+
+        public CreateAccountModel(Guid clientId, CurrencyType currencyType, AccountTypeEnum accountType)
+        {
+            ClientId = clientId;
+            CurrencyType = currencyType;
+            AccountType = accountType;
+        }
+
+        public Guid ClientId { get; set; }
+        public CurrencyType CurrencyType { get; set; } = CurrencyType.RON;
+        public AccountTypeEnum AccountType { get; set; } = AccountTypeEnum.Basic;
+    }
+}
