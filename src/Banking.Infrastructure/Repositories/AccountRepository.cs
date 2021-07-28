9e7abf53225b867291b68d013f1079962f18f7e7
t-rolfin <rolfin.dc@gmail.com>
2021-07-28 18:53:45 +0300

    Configured to create an account type when request an account form database.

--- src/Banking.Infrastructure/Repositories/AccountRepository.cs
+++ src/Banking.Infrastructure/Repositories/AccountRepository.cs
@@ -1,5 +1,7 @@
-﻿using Banking.Core.Entities;
+﻿using Banking.Core.AccountTypeFactory;
+using Banking.Core.Entities;
 using Banking.Core.Interfaces;
+using Banking.Shared.Enums;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,24 +15,34 @@ namespace Banking.Infrastructure.Repositories
     public class AccountRepository : IAccountRepository
     {
         private readonly ClientContext _context;
+        private readonly AccountTypeProviderFactory _accountTypeFactory;
 
-        public AccountRepository(ClientContext context)
+        public AccountRepository(ClientContext context, AccountTypeProviderFactory accountTypeFactory)
         {
             _context = context;
+            _accountTypeFactory = accountTypeFactory;
         }
 
         public async Task<Account> GetByIBAN(string accountIBAN)
         {
-            return await _context.Accounts.Where(x => x.IBAN == accountIBAN)
+            var account = await _context.Accounts.Where(x => x.IBAN == accountIBAN)
                 .Include(x => x.Transactions)
                 .FirstOrDefaultAsync();
+
+            SetAccountType(account);
+
+            return account;
         }
 
         public async Task<Account> GetById(Guid accountId)
         {
-            return await _context.Accounts.Where(x => x.Id == accountId)
+            var account =  await _context.Accounts.Where(x => x.Id == accountId)
                 .Include(x => x.Transactions)
                 .FirstOrDefaultAsync();
+
+            SetAccountType(account);
+
+            return account;
         }
 
         public async Task<bool> UpdateAccountList(CancellationToken cancellationToken, params Account[] accounts)
@@ -46,6 +58,14 @@ namespace Banking.Infrastructure.Repositories
             var result = await _context.SaveChangesAsync(cancellationToken);
 
             return result > 1 ? true : false;
+        }
+
+
+        void SetAccountType(Account account)
+        {
+            int accountType = (int)_context.Entry(account).Property("AccType").CurrentValue;
+
+            account.SetAccountType(_accountTypeFactory.GetAccountTypeByType((AccountTypeEnum)accountType));
         }
     }
 }
