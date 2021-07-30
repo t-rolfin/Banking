5d8e7ae8506f46629797eee4f637e3b1c18b88f3
t-rolfin <rolfin.dc@gmail.com>
2021-07-30 16:47:34 +0300

    Init wpf project.

---
+++ src/Banking.WPF/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Banking.WPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Banking.WPF
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+    }
+}
